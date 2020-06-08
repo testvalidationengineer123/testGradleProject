5dcfba709999ba84f839dae6f62540ce45924d7f
lahad diaw <testvalidationengineer123@gmail.com>
2020-06-08 14:29:56 +0200

    first commit

---
+++ src/test/java/testGradleProject/MyTest.java
@@ -0,0 +1,11 @@
+package testGradleProject;
+
+import org.junit.Test;
+
+public class MyTest {
+
+	@Test
+	public void testOne() {
+		System.out.println("////////////////// this is test one");
+	}
+}
